8a27131afa7f82dd503887ed27a7b09d9899c496
cbagt <cbagt@example.org>
2021-06-25 13:23:58 +0200

    3 TEst von Idea aus, commit MIT p tag entfernt

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home() {
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!
-                <p>Und die zweite Änderung in Idea!</p>
+                Und die zweite Änderung in Idea!</p>
           </a>
 
           <a
